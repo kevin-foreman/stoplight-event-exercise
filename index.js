ba0005d99546f36c5f6ac66262ad6fde11e6e27a
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-01-27 14:53:29 -0600

    Stop light is working

--- index.js
+++ index.js
@@ -1,5 +1,71 @@
-(function() {
-  'use strict';
+// (function () {
+//   'use strict';
+// Your code here
+let stopButton = document.getElementById("stopButton");
+let slowButton = document.getElementById("slowButton");
+let goButton = document.getElementById("goButton");
+let stopLight = document.getElementById("stopLight");
+let slowLight = document.getElementById("slowLight");
+let goLight = document.getElementById("goLight");
 
-  // YOUR CODE HERE
-})();
+
+// Stop button
+stopButton.addEventListener("click", (event) => {
+  console.log(event.target + " was clicked");
+  stopLight.classList.toggle("stop");
+  if (stopLight.classList == "bulb") {
+    console.log("Stop bulb off");
+  } else {
+    console.log("Stop bulb on");
+  };
+});
+
+
+// Slow button
+slowButton.addEventListener("click", (event) => {
+  console.log(event.target + " was clicked");
+  slowLight.classList.toggle("slow");
+  if (slowLight.classList == "bulb") {
+    console.log("Slow bulb off");
+  } else {
+    console.log("Slow bulb on");
+  };
+});
+
+// Go button
+goButton.addEventListener("click", (event) => {
+  console.log(event.target + " was clicked");
+  goLight.classList.toggle("go");
+  if (goLight.classList == "bulb") {
+    console.log("Go bulb off");
+  } else {
+    console.log("Go bulb on");
+  };
+});
+
+///////////////////////////////////////
+
+stopButton.addEventListener("mouseenter", (event) => {
+  console.log("Entered stop button");
+});
+
+stopButton.addEventListener("mouseleave", (event) => {
+  console.log("Left stop button");
+});
+
+slowButton.addEventListener("mouseenter", (event) => {
+  console.log("Entered stop button");
+});
+
+slowButton.addEventListener("mouseleave", (event) => {
+  console.log("Left stop button");
+});
+
+goButton.addEventListener("mouseenter", (event) => {
+  console.log("Entered stop button");
+});
+
+goButton.addEventListener("mouseleave", (event) => {
+  console.log("Left stop button");
+});
+// })();
